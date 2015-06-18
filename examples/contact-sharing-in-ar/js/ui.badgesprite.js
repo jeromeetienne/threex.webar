14ff4a942f3a98533ec1fb079d1e797c2dded7c5
Jerome Etienne <jerome.etienne@gmail.com>
2015-06-18 20:55:21 +0200

    more work

--- examples/contact-sharing-in-ar/js/ui.badgesprite.js
+++ examples/contact-sharing-in-ar/js/ui.badgesprite.js
@@ -1,4 +1,4 @@
-var THREEx      = THREEx        || {}
+var UI      = UI        || {}
 
 //////////////////////////////////////////////////////////////////////////////////
 //              Constructor                                                     //
@@ -7,7 +7,7 @@ var THREEx      = THREEx        || {}
 /**
  * create a plane on which we map 2d text
  */
-THREEx.BadgeSprite        = function(){
+UI.BadgeSprite        = function(){
 
         var width = 512
         var height = 256
@@ -31,44 +31,40 @@ THREEx.BadgeSprite        = function(){
 
         // Create the object
         var material = new THREE.SpriteMaterial({
+                transparent: true,
                 map     : texture
         });
         THREE.Sprite.call( this, material )
 
         this.scale.set(2,1,1)
-
-
-        this.draw(this.parameters);
 }
 
-THREEx.BadgeSprite.prototype = Object.create( THREE.Sprite.prototype );
+UI.BadgeSprite.prototype = Object.create( THREE.Sprite.prototype );
 
-THREEx.BadgeSprite.prototype.draw = function(options){
-	options		= options	|| {};
-	var params = {
-		firstName       : options.firstName !== undefined ? options.firstName	: 'John',
-		lastName        : options.lastName !== undefined ? options.lastName	: 'Doe',
-		role            : options.role !== undefined ? options.role		: 'Visitor',
-		avatar	        : options.avatar !== undefined ? options.avatar		: '/examples/tmp/avatars/avatar.jpg',
-	}
-
-
-	this.context.clearRect(0,0,this.canvas.width, this.canvas.height)
-
-
-	var context	= this.context;
-	var canvas	= this.canvas;
+/**
+ * Draw the cartouche
+ * @param  {Object} params [description]
+ */
+UI.BadgeSprite.prototype.draw = function(params){
+	var context	= this.context
+	var canvas	= this.canvas
+        var texture     = this.texture
 
 	context.save()
 
-        // TODO what this stuff is doing
-	context.fillStyle = '#fff';
-	context.strokeStyle = '#1054B5';
-	var cornerRadius = 20;
+        // clear texture
+	context.clearRect(0,0,this.canvas.width, this.canvas.height)
+
+        // Draw white background
+	var cornerRadius = 10;
+	context.fillStyle = 'rgba(255,255,255,0.5)';
+	context.fillRect(0+(cornerRadius/2), 0+(cornerRadius/2), canvas.width-cornerRadius, canvas.height-cornerRadius);
+
+        // Draw background outlining
 	context.lineJoin = 'round';
 	context.lineWidth = cornerRadius;
+	context.strokeStyle = '#1054B5';
 	context.strokeRect(0+(cornerRadius/2), 0+(cornerRadius/2), canvas.width-cornerRadius, canvas.height-cornerRadius);
-	context.fillRect(0+(cornerRadius/2), 0+(cornerRadius/2), canvas.width-cornerRadius, canvas.height-cornerRadius);
 
 	// Draw avatar
 	var avatarObject = new Image();
@@ -78,36 +74,17 @@ THREEx.BadgeSprite.prototype.draw = function(options){
 	avatarObject.style.height = '200px';
 	avatarObject.onload = function() {
 		context.drawImage(avatarObject, 10, 10, 200, 200);
+        	// make the texture as .needsUpdate
+        	texture.needsUpdate	= true;
 	};
 	avatarObject.src = params.avatar;
 	avatarObject.crossOrigin = 'Anonymous';
 
-
-	// draw outlining
+	// draw avatar outlining
 	context.rect(10,10,200,200);
 	context.lineWidth = 20;
 	context.strokeStyle = '#1054B5';
 	context.stroke();
-
-	// Draw icon
-	var iconObject = new Image();
-	iconObject.width = 50;
-	iconObject.height = 50;
-	iconObject.style.width = 50+'px';
-	iconObject.style.height = 50+'px';
-	iconObject.onload = function() {
-		context.drawImage(iconObject, 220, 170, 50, 50);
-	};
-	if(params.role === 'developer'){
-		iconObject.src = '/examples/tmp/icons/developer.png';
-	}else if(params.role === 'designer'){
-		iconObject.src = '/examples/tmp/icons/designer.png';
-	}else if(params.role === 'industry'){
-		iconObject.src = '/examples/tmp/icons/industry.png';
-	}else{
-		iconObject.src = '/examples/tmp/icons/other.png';
-	}
-	iconObject.crossOrigin = 'Anonymous';
 
 
 
@@ -115,23 +92,50 @@ THREEx.BadgeSprite.prototype.draw = function(options){
 	writeText(params.firstName,250,50,35,'normal');
 	// write Last Name
 	writeText(params.lastName,250,110,45,'bold');
-	// write role
+
+
+	// Write role label
 	writeText(params.role,270,215,50,'normal');
 
+	// Draw role icon
+	var iconObject = new Image();
+	iconObject.width = 50;
+	iconObject.height = 50;
+	iconObject.style.width = 50+'px';
+	iconObject.style.height = 50+'px';
+	iconObject.onload = function() {
+		context.drawImage(iconObject, 220, 170, 50, 50);
+
+        	// make the texture as .needsUpdate
+        	texture.needsUpdate	= true;
+	}
+	if(params.role.toLowerCase() === 'developer'){
+		iconObject.src = '/examples/contact-sharing-in-ar/role-icons/developer.png';
+	}else if(params.role.toLowerCase() === 'designer'){
+		iconObject.src = '/examples/contact-sharing-in-ar/role-icons/designer.png';
+	}else if(params.role.toLowerCase() === 'industry'){
+		iconObject.src = '/examples/contact-sharing-in-ar/role-icons/industry.png';
+	}else{
+		iconObject.src = '/examples/contact-sharing-in-ar/role-icons/other.png';
+	}
+
+
+
+        // retore context
 	context.restore()
 
 	// make the texture as .needsUpdate
-	this.texture.needsUpdate	= true;
+	texture.needsUpdate	= true;
 
         return
 
-	function writeText(text, posX, posY,size,weight){
-		context.font = weight+' '+size+'px Arial';
+	function writeText(text, positionX, positionY,size,weight){
+		context.font        = weight+' '+size+'px Arial';
 
 		context.fillStyle	= '#000';
-		context.fillText(text, posX+3, posY+3);
+		context.fillText(text, positionX+3, positionY+3);
 
 		context.fillStyle	= '#1054B5';
-		context.fillText(text, posX, posY);
+		context.fillText(text, positionX, positionY);
 	}
 }
