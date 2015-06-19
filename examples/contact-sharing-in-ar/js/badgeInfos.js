af9c2df8ed8166f3dffb1919bac4a71468f60acf
jerome etienne <jerome@jmemac.local>
2015-06-19 10:20:13 +0100

    polishing

---
+++ examples/contact-sharing-in-ar/js/badgeInfos.js
@@ -0,0 +1,24 @@
+// the 'Database' of badge infos
+var badgeInfos = [
+	{
+		firstName	: 'Mike',
+		lastName	: 'Newton',
+		role            : 'industry',
+		markerId	: 0,
+		avatar	        : '/examples/contact-sharing-in-ar/avatars/avatar-0.jpg',
+	},
+	{
+		firstName	: 'Adam',
+		lastName	: 'Huxley',
+		role            : 'designer',
+		markerId	: 236,
+		avatar	        : '/examples/contact-sharing-in-ar/avatars/avatar-236.jpg',
+	},
+	{
+		firstName	: 'Albert',
+		lastName	: 'Walton',
+		role            : 'corporate',
+		markerId	: 265,
+		avatar	        : '/examples/contact-sharing-in-ar/avatars/avatar-265.jpg',
+	},
+]
